68435359cf20d15a4e0bceaeea2ece49f7cc8cac
Ryno <rjrfourie@outlook.com>
2023-08-15 15:50:54 +0200

    Handle the draw cases

--- index.test.js
+++ index.test.js
@@ -1,4 +1,8 @@
 function rps(left, right) {
+    if (left === right) {
+        return "draw";
+      }
+      
     return (
         (left === "rock" && right === "scissors" )
         || (left === "scissors" && right === "paper")
